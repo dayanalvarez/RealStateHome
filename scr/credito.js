da84bd1ede31b6ba34436eab41ff230ea06aa5fe
DAYANA BRIYITH ALVAREZ SALAS <dalvarezsa@est.utn.ac.cr>
2022-03-27 21:05:44 -0600

    nuevo

---
+++ scr/credito.js
@@ -0,0 +1,8 @@
+﻿
+function sumar(){
+    var salario = parseInt(document.getElementById('salario').value)
+    var vivienda = parseInt(document.getElementById('vivienda').value)
+    
+    var result = salario + vivienda;
+    document.getElementById('resultado').value = result;
+}
